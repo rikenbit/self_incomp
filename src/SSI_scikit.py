b7cef3b74fe664f9e373a7f1dc65e07e573582e8
yamaken37 <samadakenji@gmail.com>
2022-12-05 15:19:57 +0900

    default, n_estimators=10000 , timestamp

--- src/SSI_scikit.py
+++ src/SSI_scikit.py
@@ -1,4 +1,39 @@
-####データ全部使う#########
+# #### default#########
+# import pandas as pd
+# import numpy as np
+# #ランダムフォレスト
+# from sklearn.ensemble import RandomForestClassifier
+# #ホールドアウトのimport
+# from sklearn.model_selection import train_test_split
+# #交差検証のimport
+# from sklearn.model_selection import cross_val_score
+# #LeaveOneOutのimport
+# from sklearn.model_selection import LeaveOneOut
+
+# # データ読み込み
+# x_csv = pd.read_csv("./output/SSI/x_r.csv")
+# y_csv = pd.read_csv("./output/SSI/y_r.csv")
+
+# # 実データのベクトルY
+# y = y_csv["x"].values
+# # 実データのベクトルX
+# X = x_csv.values
+
+
+# # ランダムフォレスト
+# clf = RandomForestClassifier(random_state=1234)
+
+# # LeaveOneOutの作成
+# loocv = LeaveOneOut()
+
+# # LOOCV
+# score = cross_val_score(clf, X, y, cv=loocv)
+
+# # LOOCVの結果 https://panda-clip.com/loocv/
+# print("{:.4f}".format(np.mean(score)))
+# ################################################################
+
+#### ハイパラ選定#################################################
 import pandas as pd
 import numpy as np
 #ランダムフォレスト
@@ -21,18 +56,21 @@ X = x_csv.values
 
 
 # ランダムフォレスト
-clf = RandomForestClassifier(random_state=1234, n_estimators=1000)
+clf = RandomForestClassifier(random_state=1234, n_estimators=10000)
 
 # LeaveOneOutの作成
 loocv = LeaveOneOut()
 
 # LOOCV
+# 時間計測開始
+time_sta = time.time()
 score = cross_val_score(clf, X, y, cv=loocv)
+# 時間計測終了
+time_end = time.time()
+# 経過時間（秒）
+tim = time_end- time_sta
+print(tim)
 
 # LOOCVの結果 https://panda-clip.com/loocv/
 print("{:.4f}".format(np.mean(score)))
-################################################################
-
-#### ハイパラ選定#################################################
-
 ################################################################
