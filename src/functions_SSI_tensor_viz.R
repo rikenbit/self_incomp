32899ed73a069b7afab0d7ba28374401395a25b4
yamaken37 <samadakenji@gmail.com>
2022-12-26 23:49:39 +0900

    fix for new file name

--- src/functions_SSI_tensor_viz.R
+++ src/functions_SSI_tensor_viz.R
@@ -5,52 +5,57 @@ library(tidyverse)
 # ###################for文#####################
 # #### colnames from ファイル名####
 # cv_filename <- cv_filenames[1]
-# cv_filename |> 
-#   str_remove(".csv") |> 
-#   strsplit("_") |> 
-#   unlist() |> 
-#   str_extract_all("[:alpha:]+") |> 
+# cv_filename |>
+#   str_remove(".csv") |>
+#   strsplit("_") |>
+#   unlist() |>
+#   str_extract_all("[:alpha:]+") |>
 #   unlist() -> cv_colnames
-# 
+#
 # #### parametor from ファイル名####
-# cv_filename |> 
-#   strsplit("_") |> 
-#   unlist() |> 
-#   str_extract_all("[:digit:]+") |> 
+# cv_filename |>
+#   strsplit("_") |>
+#   unlist() |>
+#   str_extract_all("[:digit:]+") |>
 #   unlist() -> cv_params
-# 
+#
 # #### dataframe parametor####
-# rbind(cv_colnames, cv_params) |> 
+# rbind(cv_colnames, cv_params) |>
 #   as.data.frame(col.names = cv_colnames,
 #                 row.names = FALSE) -> cv_df
 # colnames(cv_df) <- cv_df[1,]
 # cv_df <- cv_df[2,]
-# 
+#
 # #### dataframe LOOCV####
 # cv_dirname <- cv_dirnames[1]
 # cv_value <- read_csv(cv_dirname, col_names = "loocv")
-# 
+#
 # #### cbind####
 # df_one_loocv <- merge(cv_df, cv_value)
 # ###################for文#####################
 
 .df_onerow  = function(i) {
   #### colnames from ファイル名####
-  cv_filename <- cv_filenames[i]
-  cv_filename |> 
-    str_remove(".csv") |> 
-    strsplit("_") |> 
-    unlist() |> 
-    str_extract_all("[:alpha:]+") |> 
-    unlist() -> cv_colnames
+  # cv_filename <- cv_filenames[i]
+  # cv_filename |>
+  #   str_remove(".csv") |>
+  #   strsplit("_") |>
+  #   unlist() |>
+  #   str_extract_all("[:alpha:]+") |>
+  #   unlist() -> cv_colnames
+  cv_filenames[i] |>
+      strsplit("_") |>
+          unlist() -> cv_col_vec
+      cv_col_vec[seq(1,length(cv_col_vec),2)] -> cv_colnames
   #### parametor from ファイル名####
-  cv_filename |> 
-    strsplit("_") |> 
-    unlist() |> 
-    str_extract_all("[:digit:]+") |> 
-    unlist() -> cv_params
+  # cv_filename |>
+  #   strsplit("_") |>
+  #   unlist() |>
+  #   str_extract_all("[:digit:]+") |>
+  #   unlist() -> cv_params
+      cv_col_vec[seq(2,length(cv_col_vec),2)] -> cv_params
   #### dataframe parametor####
-  rbind(cv_colnames, cv_params) |> 
+  rbind(cv_colnames, cv_params) |>
     as.data.frame(col.names = cv_colnames,
                   row.names = FALSE) -> cv_df
   colnames(cv_df) <- cv_df[1,]
@@ -60,7 +65,7 @@ library(tidyverse)
   cv_value <- read_csv(cv_dirname, col_names = "Loocv")
   #### cbind####
   df_one_loocv <- merge(cv_df, cv_value)
-  
+
   df_one_loocv-> return_object
   return(return_object)
 }
