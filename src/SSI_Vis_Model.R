d8269c9944dec561dc59fac7d19096c7b7417d89
yamaken37 <samadakenji@gmail.com>
2023-01-12 15:50:51 +0900

    fix SSI_Vis_Model plot

--- src/SSI_Vis_Model.R
+++ src/SSI_Vis_Model.R
@@ -3,16 +3,24 @@ source("src/functions_SSI_Vis_Model.R")
 #### args setting####
 args <- commandArgs(trailingOnly = T)
 args_input_dir <- args[1]
-args_output <- args[2]
-args_output_fw <- args[3]
-args_output_dim <- args[4]
-args_output_para <- args[5]
+
+args_output_Dens <- args[2]
+args_output_Dens_FW <- args[3]
+args_output_DimX <- args[4]
+args_output_Para <- args[5]
+args_output_Hist_Id <- args[6]
+args_output_Hist_Fill <- args[7]
+args_Hist_Id_FW <- args[8]
 #### test args####
 # args_input_dir  <- c("output/LOOCV_rf")
-# args_output    <- c("output/Vis_Model/Acc_Dens_plot.png")
-# args_output_fw <- c("output/Vis_Model/Acc_Dens_plot_FW.png")
-# args_output_dim <- c("output/Vis_Model/Dim_X_plot.png")
-# args_output_para <- c("output/Vis_Model/Num_Para_plot.png")
+
+# args_output_Dens    <- c("output/Vis_Model/Dens.png")
+# args_output_Dens_FW <- c("output/Vis_Model/Dens_FW.png")
+# args_output_DimX <- c("output/Vis_Model/DimX.png")
+# args_output_Para <- c("output/Vis_Model/NumPara.png")
+# args_output_Hist_Id <- c("output/Vis_Model/Hist_Id.png")
+# args_output_Hist_Fill <- c("output/Vis_Model/Hist_Fill.png")
+# args_Hist_Id_FW <- c("output/Vis_Model/Hist_Id_FW.png")
 
 #### ディレクトリ一覧取得####
 cv_dirnames <- list.files(args_input_dir, full.names = TRUE)
@@ -22,7 +30,7 @@ cv_filenames <- list.files(args_input_dir, full.names = FALSE)
 #### create dataframe####
 purrr::map_dfr(seq(length(cv_filenames)), .df_onerow) |>
     dplyr::mutate(All_Tensor_Params = str_remove(cv_filenames, ".csv"), .before=1) -> df_loocv
-
+# http://www.g36cmsky.com/archives/28118887.html
 new_order <- c("Model-1-A1G",
                "Model-1-A1",
                "Model-2-A1G",
@@ -42,11 +50,11 @@ new_order <- c("Model-1-A1G",
                "Model-11-A1A4GLGR",
                "Model-11-A1A4",
                "Model-PCA")
+df_loocv$MODELS <- factor(df_loocv$MODELS, levels=new_order)
 
 #### ggplot density group scaled by count ####
 cord_x <- c("Accuracy")
-cord_y <- c("density (scaled)")
-ggplot_title <- c("Density plot:Accuracy")
+cord_y <- c("Density (scaled)")
 
 gg_acc <- ggplot(df_loocv,
                  aes(x = Loocv,
@@ -57,16 +65,14 @@ gg_acc <- ggplot(df_loocv,
                  ) +
   geom_density(linewidth = 4.5) +
   labs(x = cord_x,
-       y = cord_y,
-       title = ggplot_title
+       y = cord_y
        ) +
-  scale_x_continuous(breaks = seq(0, 1, 0.2), limits = c(0, 1)) +
+  scale_x_continuous(breaks = seq(0.2, 0.9, 0.1), limits = c(0.2, 0.9)) +
   ylim(c(0, 2)) +
   theme(plot.title = element_text(hjust = 0.5)) +
-  theme(text = element_text(size = 90)) +
-  scale_color_discrete(breaks = new_order)
+  theme(text = element_text(size = 90))
 
-ggsave(filename = args_output,
+ggsave(filename = args_output_Dens,
        plot = gg_acc,
        dpi = 50,
        width = 50.0,
@@ -75,27 +81,23 @@ ggsave(filename = args_output,
 
 #### ggplot density facet_wrap ####
 cord_x <- c("Accuracy")
-cord_y <- c("density (y_scale free)")
-ggplot_title <- c("Density plot:Accuracy  (each Model)")
+cord_y <- c("Density (y_scale free)")
 
 gg_acc_fw <- ggplot(df_loocv,
                  aes(x = Loocv,
                      col = MODELS)) +
     geom_density(linewidth = 4.5) +
-    scale_color_discrete(breaks = new_order) +
     labs(x = cord_x,
-         y = cord_y,
-         title = ggplot_title,
-         col = "MODELS"
+         y = cord_y
          ) +
-    scale_x_continuous(breaks = seq(0, 1, 0.2), limits = c(0, 1)) +
+    scale_x_continuous(breaks = seq(0.2, 0.9, 0.2), limits = c(0.2, 0.9)) +
     theme(plot.title = element_text(hjust = 0.5)) +
     theme(text = element_text(size = 90)) +
     facet_wrap(~factor(MODELS, levels=new_order),
                ncol = 4,
                scales = "free")
 
-ggsave(filename = args_output_fw,
+ggsave(filename = args_output_Dens_FW,
        plot = gg_acc_fw,
        dpi = 50,
        width = 65.0,
@@ -115,7 +117,6 @@ df_model_dim_acc <- dplyr::select(df_loocv_dim,
                                   Loocv)
 cord_x <- c("Dimention of X")
 cord_y <- c("Accuracy")
-ggplot_title <- c("plot Dimention of X")
 
 gg_dim <- ggplot(df_model_dim_acc,
              aes(x = dim_exp_x,
@@ -123,19 +124,15 @@ gg_dim <- ggplot(df_model_dim_acc,
                  col = MODELS
                  )
              ) +
-    geom_point(size = 6.0,
-               alpha = 0.6) +
-    scale_color_discrete(breaks = new_order) +
-    scale_y_continuous(breaks = seq(0, 1, 0.2), limits = c(0, 1)) +
+    geom_point(size = 6.0) +
+    scale_y_continuous(breaks = seq(0.2, 0.9, 0.1), limits = c(0.2, 0.9)) +
     labs(x = cord_x,
-         y = cord_y,
-         title = ggplot_title,
-         col = "MODELS"
+         y = cord_y
          ) +
     theme(plot.title = element_text(hjust = 0.5)) +
     theme(text = element_text(size = 90))
 
-ggsave(filename = args_output_dim,
+ggsave(filename = args_output_DimX,
        plot = gg_dim,
        dpi = 50,
        width = 50.0,
@@ -153,7 +150,6 @@ df_loocv |>
 
 cord_x <- c("Number of Parameters")
 cord_y <- c("Accuracy")
-ggplot_title <- c("plot Number of Parameters")
 
 gg_para <- ggplot(df_model_para_acc,
                  aes(x = num_para,
@@ -161,22 +157,92 @@ gg_para <- ggplot(df_model_para_acc,
                      col = MODELS
                      )
                  ) +
-    geom_point(size = 6.0,
-               alpha = 0.6) +
-    scale_color_discrete(breaks = new_order) +
-    scale_y_continuous(breaks = seq(0, 1, 0.2), limits = c(0, 1)) +
+    geom_point(size = 6.0) +
+    scale_y_continuous(breaks = seq(0.2, 0.9, 0.1), limits = c(0.2, 0.9)) +
     scale_x_continuous(breaks = seq(1, 6, 1), limits = c(1, 6)) +
     labs(x = cord_x,
-         y = cord_y,
-         title = ggplot_title,
-         col = "MODELS"
+         y = cord_y
          ) +
     theme(plot.title = element_text(hjust = 0.5)) +
     theme(text = element_text(size = 90))
 
-ggsave(filename = args_output_para,
+ggsave(filename = args_output_Para,
        plot = gg_para,
        dpi = 50,
        width = 50.0,
        height = 40.0,
+       limitsize = FALSE)
+#### ggplot hist group id####
+cord_x <- c("Accuracy")
+cord_y <- c("Count")
+
+gg_hist <- ggplot(df_loocv,
+                 aes(x = Loocv,
+                     fill = MODELS
+                     )
+                 ) +
+    # https://best-biostatistics.com/excel/sturges.html スタージェス ヒストグラムの階級数(ビンの数)
+    geom_histogram(position="identity", alpha=0.8) +
+    # geom_histogram(position="fill")+
+    labs(x = cord_x,
+         y = cord_y
+         ) +
+    scale_x_continuous(breaks = seq(0.2, 0.9, 0.1), limits = c(0.2, 0.9)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(text = element_text(size = 90))
+
+ggsave(filename = args_output_Hist_Id,
+       plot = gg_hist,
+       dpi = 50,
+       width = 50.0,
+       height = 40.0,
+       limitsize = FALSE)
+#### ggplot hist group fill####
+cord_x <- c("Accuracy")
+cord_y <- c("Ratio")
+
+gg_hist_fill <- ggplot(df_loocv,
+                  aes(x = Loocv,
+                      fill = MODELS
+                      )
+                  ) +
+  # https://best-biostatistics.com/excel/sturges.html スタージェス ヒストグラムの階級数(ビンの数)
+  geom_histogram(position="fill")+
+  labs(x = cord_x,
+       y = cord_y
+  ) +
+  scale_x_continuous(breaks = seq(0.2, 0.9, 0.1), limits = c(0.2, 0.9)) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(text = element_text(size = 90))
+
+ggsave(filename = args_output_Hist_Fill,
+       plot = gg_hist_fill,
+       dpi = 50,
+       width = 50.0,
+       height = 40.0,
+       limitsize = FALSE)
+#### ggplot hist facet_wrap ####
+cord_x <- c("Accuracy")
+cord_y <- c("Count")
+
+gg_hist_fw <- ggplot(df_loocv,
+                     aes(x = Loocv,
+                         fill = MODELS)
+                     ) +
+  geom_histogram(position="identity") +
+  labs(x = cord_x,
+       y = cord_y
+  ) +
+  scale_x_continuous(breaks = seq(0.2, 0.9, 0.2), limits = c(0.2, 0.9)) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(text = element_text(size = 90)) +
+  facet_wrap(~MODELS,
+             ncol = 4,
+             scales = "free")
+
+ggsave(filename = args_Hist_Id_FW,
+       plot = gg_hist_fw,
+       dpi = 50,
+       width = 65.0,
+       height = 50.0,
        limitsize = FALSE)
--- workflow/SSI_Vis_Model.smk
+++ workflow/SSI_Vis_Model.smk
@@ -2,29 +2,32 @@
 ###################################################
 rule all:
     input:
-        'output/Vis_Model/Acc_Dens_plot.png',
-        'output/Vis_Model/Acc_Dens_plot_FW.png',
-        'output/Vis_Model/Dim_X_plot.png',
-        'output/Vis_Model/Num_Para_plot.png'
+        'output/Vis_Model/Dens.png',
+        'output/Vis_Model/Dens_FW.png',
+        'output/Vis_Model/DimX.png',
+        'output/Vis_Model/NumPara.png',
+        'output/Vis_Model/Hist_Id.png',
+        'output/Vis_Model/Hist_Fill.png',
+        'output/Vis_Model/Hist_Id_FW.png'
 
 rule SSI_Vis_Model:
-    # input:
-    #     'output/X_Tensor/{list_l}.csv',
-    #     'output/SSI/y_r.csv'
     output:
-        'output/Vis_Model/Acc_Dens_plot.png',
-        'output/Vis_Model/Acc_Dens_plot_FW.png',
-        'output/Vis_Model/Dim_X_plot.png',
-        'output/Vis_Model/Num_Para_plot.png'
+        'output/Vis_Model/Dens.png',
+        'output/Vis_Model/Dens_FW.png',
+        'output/Vis_Model/DimX.png',
+        'output/Vis_Model/NumPara.png',
+        'output/Vis_Model/Hist_Id.png',
+        'output/Vis_Model/Hist_Fill.png',
+        'output/Vis_Model/Hist_Id_FW.png'
     params:
         input_dir = 'output/LOOCV_rf'
     benchmark:
-        'benchmarks/Vis_Model/Acc_Dens_plot.txt'
+        'benchmarks/Vis_Model/Dens.txt'
     container:
         'docker://yamaken37/ssi_vis_pairplot:20230106'
     resources:
         mem_gb=200
     log:
-        'logs/Vis_Model/Acc_Dens_plot.log'
+        'logs/Vis_Model/Dens.log'
     shell:
         'src/SSI_Vis_Model.sh {params.input_dir} {output} >& {log}'
