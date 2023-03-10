e38479422558d1ce03f3406bf86e91c774fcefbe
yamaken37 <samadakenji@gmail.com>
2023-03-10 14:09:52 +0900

    SSI_Vis_train_180row.smk test

--- src/SSI_Vis_train_180row.R
+++ src/SSI_Vis_train_180row.R
@@ -2,45 +2,127 @@ source("src/functions_SSI_Vis_train_180row.R")
 
 #### args setting####
 args <- commandArgs(trailingOnly = T)
-args_input <- args[1]
-args_rec_error <- args[2]
-args_rel_change <- args[3]
+args_input_pattern <- args[1]
+
+args_input_dir <- args[2]
+args_input_dir2 <- args[3]
+
+args_rec_error <- args[4]
+args_rec_error_F <- args[5]
+args_rel_change <- args[6]
+args_rel_change_F <- args[7]
+
 #### test args####
-args_input_dir <- c("output/toy/MT_train_X/tensor")
-args_input_dir2 <- c("output/toy/MT_train_X/tensor/")
-args_input_pattern <- c("MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row")
+# args_input_dir <- c("output/toy/MT_train_X/tensor")
+# args_input_dir2 <- c("output/toy/MT_train_X/tensor/")
+# args_input_pattern <- c("MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row")
+# 
+# args_rec_error <- c("output/toy/Vis_train/MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row.png")
+# args_rec_error_F <- c("output/toy/Vis_train_F1/MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row.png")
+# 
+# args_rel_change <- c("output/toy/Vis_train/MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row.png")
+# args_rel_change_F <- c("output/toy/Vis_train_F1/MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row.png")
 
-args_rel_change <- c("output/toy/Vis_train/MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row.png")
-args_rel_change_F <- c("output/toy/Vis_train_F1/MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row.png")
-
-args_rec_error <- c("output/toy/Vis_train/MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row.png")
-args_rec_error_F <- c("output/toy/Vis_train_F1/MODELS_Model-1-A1G_r1_10_r2_20_r3_10_r1L_xx_r1R_xx_r2L_xx_r2R_xx_r3L_xx_r3R_xx_row.png")
-
-# get dirname
+#### get dirname####
 list.files(args_input_dir,
            pattern = args_input_pattern,
            full.names = TRUE) |>
     str_subset(pattern = ".RData") -> cv_dirnames
-# get filename
+#### get filename####
 list.files(args_input_dir,
            pattern = args_input_pattern,
            full.names = FALSE) |>
     str_subset(pattern = ".RData") -> cv_filenames
-# get sort order
+#### get sort order####
 cv_filenames |>
     str_remove(args_input_pattern) |>
-    str_remove("_") |>
-    str_remove(".RData") |>
-    as.numeric() |>
-    sort() -> cv_sort
+        str_remove("_") |>
+            str_remove(".RData") |>
+                as.numeric() |>
+                    sort() -> cv_sort
 
-# file pass list ,sort by row num
+#### file pass list ,sort by row num####
 input_path_list <- c()
 for(i in cv_sort){
     eval(parse(text=paste0("path <- c('", args_input_dir2, args_input_pattern, "_", i, ".RData')")))
     input_path_list <- c(input_path_list, path)
 }
 
+#### rec_error####
+# purrr
+purrr::map_dfc(cv_sort, .df_rec_error) |>
+    rownames_to_column("order") -> df_rec_error
+# pivot_longer
+df_rec_error |>
+    pivot_longer(!order,
+                 names_to = "Pullout_row",
+                 values_to = "RecError") -> df_rec_error_long
+
+row_order <- c()
+for (i in cv_sort) {
+    eval(parse(text=paste0("row_order <- c(row_order,'row_",i,"')")))
+}
+order_order <- df_rec_error$order
+
+gg_rec_error <- ggplot(df_rec_error_long,
+                       aes(x = factor(order, levels = order_order),
+                           y = RecError,
+                           color = factor(Pullout_row, levels = row_order)
+                       )
+) +
+    geom_point(size=2.0, alpha = 0.6) +
+    labs(title = "RecError",
+         color="Pullout_row",
+         x = "order") +
+    theme(text = element_text(size = 60)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(
+        legend.position = c(1, 1),
+        legend.justification = c(1, 1),
+        legend.text = element_text(size=30),
+        legend.title = element_text(size=30)
+    ) +
+    scale_y_log10()
+#### ggsave rec_error####
+ggsave(filename = args_rec_error,
+       plot = gg_rec_error,
+       dpi = 100,
+       width = 30.0,
+       height = 30.0,
+       limitsize = FALSE)
+
+#### rec_error_F####
+# filter
+df_rec_error_long |> dplyr::filter(order!="1") -> df_rec_error_long_F
+
+gg_rec_error_F <- ggplot(df_rec_error_long_F,
+                         aes(x = factor(order, levels = order_order),
+                             y = RecError,
+                             color = factor(Pullout_row, levels = row_order)
+                         )
+) +
+    geom_point(size=2.0, alpha = 0.6) +
+    labs(title = "RecError",
+         color="Pullout_row",
+         x = "order") +
+    theme(text = element_text(size = 60)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(
+        legend.position = c(1, 1),
+        legend.justification = c(1, 1),
+        legend.text = element_text(size=30),
+        legend.title = element_text(size=30)
+    ) +
+    scale_y_log10()
+#### ggsave rec_error_F####
+ggsave(filename = args_rec_error_F,
+       plot = gg_rec_error_F,
+       dpi = 100,
+       width = 30.0,
+       height = 30.0,
+       limitsize = FALSE)
+
+#### rel_change####
 # purrr
 purrr::map_dfc(cv_sort, .df_rel_change) |>
     rownames_to_column("order") -> df_rel_change
@@ -63,7 +145,6 @@ gg_rel_change <- ggplot(df_rel_change_long,
                             )
                         ) +
     geom_point(size=2.0, alpha = 0.6) +
-    # theme(legend.position = "none") +
     labs(title = "RelChange",
          color="Pullout_row",
          x = "order") +
@@ -76,7 +157,7 @@ gg_rel_change <- ggplot(df_rel_change_long,
         legend.title = element_text(size=30)
         ) +
     scale_y_log10()
-#### ggsave####
+#### ggsave rel_change####
 ggsave(filename = args_rel_change,
        plot = gg_rel_change,
        dpi = 100,
@@ -84,10 +165,11 @@ ggsave(filename = args_rel_change,
        height = 30.0,
        limitsize = FALSE)
 
+#### rel_change####
 # filter
 df_rel_change_long |> dplyr::filter(order!="1") -> df_rel_change_long_F
 
-gg_rel_change_F <- ggplot(df_rel_change_long,
+gg_rel_change_F <- ggplot(df_rel_change_long_F,
                           aes(x = factor(order, levels = order_order),
                               y = RelChange,
                               color = factor(Pullout_row, levels = row_order)
@@ -106,10 +188,13 @@ gg_rel_change_F <- ggplot(df_rel_change_long,
         legend.title = element_text(size=30)
     ) +
     scale_y_log10()
-#### ggsave####
+#### ggsave rel_change_F####
 ggsave(filename = args_rel_change_F,
        plot = gg_rel_change_F,
        dpi = 100,
        width = 30.0,
        height = 30.0,
        limitsize = FALSE)
+
+
+
