43a0f2bbcb870e9f3ba09958851d35a388b17a40
yamaken37 <samadakenji@gmail.com>
2023-05-17 00:39:57 +0900

    SSI_cfa_180row BraxBra

---
+++ src/SSI_cfa_180row.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#$ -l nc=4
+#$ -p -50
+#$ -r yes
+#$ -q large.q
+
+#SBATCH -n 4
+#SBATCH --nice=50
+#SBATCH --requeue
+#SBATCH -p node03-06
+SLURM_RESTART_COUNT=2
+
+# conda
+# echo $@
+# echo $CONDA_PREFIX
+# $CONDA_PREFIX/bin/Rscript src/SSI_cfa_180row.R $@
+
+# docker
+echo $@
+Rscript src/SSI_cfa_180row.R $@
